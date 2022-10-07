7f2ac9c4c52f7179c6b123851c7223c36ff62140
durgi <durgi@leela>
2022-10-07 13:02:27 +0530

    Text execution

---
+++ src/test/java/yuvraj/sindhu.java
@@ -0,0 +1,19 @@
+package yuvraj;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterTest;
+
+public class sindhu {
+  @Test
+  public void f() {
+  }
+  @BeforeTest
+  public void beforeTest() {
+  }
+
+  @AfterTest
+  public void afterTest() {
+  }
+
+}
